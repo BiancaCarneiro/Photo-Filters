984942fbd3cca8b3d95a577b82e6af81c42a2cf8
BiancaCarneiro <bianca.ccnf@hotmail.com>
2021-09-30 19:34:17 -0300

    Color detector

--- photo_filters.py
+++ photo_filters.py
@@ -3,13 +3,22 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdi
 import sys
 
 
-
-
 image = "pictures\pic3.jpg"
 img = cv2.imread(image)
+r = g = b = 0
+clicked = False
 
 def exit_app():
     app.quit()
+
+def draw_function(event, x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, clicked
+        clicked = True
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
 
 class LoginForm(QWidget):
     # Defines the structure of the app
@@ -34,6 +43,11 @@ class LoginForm(QWidget):
 
         button_signup = QPushButton('To sketch')
         button_signup.clicked.connect(self.to_sketch)
+        layout.addWidget(button_signup, 3, 0, 1, 1)
+        layout.setRowMinimumHeight(2, 25)
+
+        button_signup = QPushButton('Check color')
+        button_signup.clicked.connect(self.check_color)
         layout.addWidget(button_signup, 3, 1, 1, 1)
         layout.setRowMinimumHeight(2, 25)
 
@@ -67,7 +81,22 @@ class LoginForm(QWidget):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-
+    def check_color(self):
+        global clicked
+        msg = QMessageBox()
+        cv2.namedWindow('Check Color')
+        cv2.setMouseCallback('Check Color', draw_function)
+        while 1:
+            cv2.imshow("Check Color", img)
+            if clicked:
+                print(r, g, b)
+                msg.setText("R: " + str(r) + "   G: " + str(g) + "   B: " + str(b))
+                #msg.setStyleSheet("text-color: rgb(255, 255, 255);")
+                msg.exec_()
+                clicked = False
+            # EXITS WITH ESC + X
+            if cv2.waitKey(20) & 0xFF ==27:
+                break
 
 
 if __name__ == "__main__":
